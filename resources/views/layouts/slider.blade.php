8b97a1996bf347cd4bcfe18b3bebfc0ba2551650
Rajesh Budhathoki <rajoyish@gmail.com>
2023-01-12 08:45:40 +0545

    About us and slider finish up

--- resources/views/layouts/slider.blade.php
+++ resources/views/layouts/slider.blade.php
@@ -1,18 +1,18 @@
 <section class="w-full overflow-hidden flex flex-nowrap text-center items-start text-red-900 font-bold" id="slider">
     <div class="flex-none w-full flex flex-col items-center justify-center">
-        <img src="https://picsum.photos/id/296/1000/350" class="object-cover w-full h-full">
+        <img src="{{ asset('images/sliders/slider-1.JPG') }}" class="object-cover w-full h-full">
         <p class="text-lg mt-4 md:text-2xl font-bo font-normal">
             ༄༅། །ཀཿཐོག་རྣམ་སྣང་འོད་གསལ་ཆོས་གླིང་།
         </p>
     </div>
     <div class="flex-none w-full flex flex-col items-center justify-center">
-        <img src="https://picsum.photos/id/306/1000/350" class="object-cover w-full h-full">
+        <img src="{{ asset('images/sliders/slider-2.png') }}" class="object-cover w-full h-full">
         <p class="text-md mt-4 md:text-2xl px-2">
             काथोग नमनङ होसेल छोलिङ गुम्बा
         </p>
     </div>
     <div class="flex-none w-full flex flex-col items-center justify-center">
-        <img src="https://picsum.photos/id/631/1000/350" class="object-cover w-full h-full">
+        <img src="{{ asset('images/sliders/slider-3.JPG') }}" class="object-cover w-full h-full">
         <p class="text-lg mt-4 md:text-2xl">
             Kathog Namnang Hosel Chholing Monastery
         </p>
